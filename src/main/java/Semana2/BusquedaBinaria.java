fda9629b492c89e8cbb25d33a757488c5699cfb5
Carlos <Carlos@HAC-001>
2019-10-25 23:14:39 -0600

    Agrega de manera aleatoria 1000 valores a un arreglo y por eso tarda en cargar

--- src/main/java/Semana2/BusquedaBinaria.java
+++ src/main/java/Semana2/BusquedaBinaria.java
@@ -11,16 +11,15 @@ import java.util.Scanner;
  */
 public class BusquedaBinaria {
     
-    
     static void imprimirArreglo(int arreglo[]){
         for(int valor:arreglo){
-            System.out.printf("[%d] ",valor);
+            System.out.printf("[%d] \n",valor);
         }
         System.out.println("");
         
     }
-    
-    static int busquedaBinaria(int buscar,int arreglo[],int inicio,int fin){
+    //Funcion recursiva
+    static int busquedaBinaria(int buscar,int arreglo[],int inicio,int fin)throws InterruptedException{
         //Caso base
         if(fin<inicio)
             return -1;
@@ -32,33 +31,91 @@ public class BusquedaBinaria {
         
         else if(buscar>arreglo[pivote])
             return busquedaBinaria(buscar,arreglo,pivote+1,fin);
-        else 
-            return busquedaBinaria(buscar,arreglo,inicio,pivote-1);
-        
-        
+        else
+            return busquedaBinaria(buscar,arreglo,inicio,pivote-1); 
     }
     
-    public static void main(String[] args) {
+    //Funcion lineal
+    static int busquedaBinaria2(int busqueda,int arreglo[],int respuesta){
+       int posicion=0;
+       
+       for(int busco:arreglo){
+           if(busco==busqueda){
+                respuesta=posicion;
+                return respuesta;
+       }
+           posicion++;
+    }
+        return respuesta;
+    }
+    
+    public static void main(String[] args) throws InterruptedException {
+        
+       
         Scanner lea=new Scanner(System.in);
         System.out.println("***B U S Q U E D A  B I N A R I A***");
-        int arreglo[]=new int[10];
+        BusquedaBinaria llamada=new BusquedaBinaria();
+        int auxiliar,opcion=0;
+        int arreglo[]=new int[1000];
+        int[] arregloOrdenado=new int[1000];
+        arreglo[0]=(int)(Math.random()*1000);
         
-        for(int x=0;x<arreglo.length;x++){
-            System.out.printf("Ingrese valor en array[%d]: ",x);
-            arreglo[x]=lea.nextInt();
+        //INICIO Ingreso de valores RANDOM
+        for(int x=1;x<arreglo.length;x++){
+            arreglo[x]=(int)(Math.random()*10);
+            for(int y=0;y<x;y++){
+                if(arreglo[x]==arreglo[y]){
+                    x--;
+                }
+            }
         }
+        //FIN Ingreso de valores RANDOM
         
-        imprimirArreglo(arreglo);
+        //INICIO Algoritmo Burbuja
+        for(int i = 0; i < arreglo.length-1; i++){
+        for(int j = 0;j < arreglo.length-1;j++){
+          if(arreglo[j] > arreglo[j+1]){
+            auxiliar = arreglo[j];
+            arreglo[j] = arreglo[j+1];
+            arreglo[j+1] = auxiliar;
+          }   
+         }
+        }
+         arregloOrdenado = arreglo;
+        //FIN algoritmo burbuja
+        
+        imprimirArreglo(arregloOrdenado);
+        
+       do{ 
+        
         
         System.out.println("Ingrese valor a buscar: ");
         int buscar=lea.nextInt();
         
         int busqueda= busquedaBinaria(buscar,arreglo,0,arreglo.length);
+        int busqueda2=busquedaBinaria2(buscar,arregloOrdenado,9);
         
+        long inicio= System.nanoTime();
         if(busqueda==-1)
             System.out.println("Valor no encontrado en arreglo");
-        else
-            System.out.printf("Valor encontradp en arreglo[%d] %n",busqueda);
+        else{
+            System.out.printf("Busqueda Binaria: Valor encontrad0 en arreglo[%d] %n",busqueda);
+            long fin= System.nanoTime();
+            double tiempo=(double)((fin-inicio));
+            System.out.println(tiempo+ " nano segundos");
+            System.out.printf("Busqueda lineal: Valor encontrad0 en arreglo[%d] %n",busqueda2);
+            fin= System.nanoTime();
+            double tiempo2=(double)((fin-inicio));
+            System.out.println(tiempo2+ " nano segundos");
+            System.out.println("La busqueda lineal se tarda " +(tiempo2-tiempo)+ " nano segundos mas que la busqueda binaria.");
+            tiempo=0;
+            tiempo2=0;
+            
+            System.out.println("\nDesea buscar otro valor? (1.SI  2.NO)");
+            opcion=lea.nextInt();
+        }
+       }while(opcion!=2);
+        
     }
     
     
